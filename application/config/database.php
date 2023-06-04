17c3bde94b6cbee88450e9cf108b4e1d1288164c
Normandia Akbar <normnd.akbr2@gmail.com>
2023-06-04 10:17:39 +0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -77,8 +77,8 @@ $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'postgres',
-	'password' => 'kresnavw210101',
-	'database' => 'bank_data_karyawan',
+	'password' => '12345',
+	'database' => 'test_kary',
 	'dbdriver' => 'postgre',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
